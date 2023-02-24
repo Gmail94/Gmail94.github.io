4d0f7243506fcba8b341c4ab1798648c8eb3b2d4
Liam Gilmore <liamgilmore1@gmail.com>
2023-02-24 10:59:46 -0500

    back2top btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,3 +132,19 @@ form.addEventListener("submit", function(event) {
     // Open the default email client with the email message as a URL
     window.location.href = emailMessage;
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	document.getElementById("back-to-top").style.display = "block";
+  } else {
+	document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
